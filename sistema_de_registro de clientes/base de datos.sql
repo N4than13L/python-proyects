7679aaf527c6ce198025d9a15513f3beb55ff815
jose nathaniel bonilla <natharevolution@gmail.com>
2021-01-23 22:49:35 -0400

    refrescando aplicacion de negocio e inventario con python

--- sistema_de_registro de clientes/base de datos.sql
+++ sistema_de_registro de clientes/base de datos.sql
@@ -1,20 +1,29 @@
-create table if not exists Usuarios(
-	id int unsigned not null,
-	nombre varchar(20),
-    apellido varchar(20)
+create database if not exists negocio_de_pulgas;
+use negocio_de_pulgas;
+#----------------------------------------------------------------------------------------------------
+create table if not exists Empleados(
+	Cedula int not null,
+	nombre varchar(15) not null,
+    apellido varchar(15) not null,
+    rango varchar(15) not null,
+    fecha_de_nacimiento varchar(50)
+);
+drop table Empleados;
+#-----------------------------------------------------------------------------------------------------
+create table if not exists Clientes(
+	nombre varchar(50),
+    apodo varchar(50),
+    Cedula int,
+    Saldo_a_deber float
+);
+#------------------------------------------------------------------------------------------------------
+create table if not exists productos(
+	_Key int not null,
+	fabricante varchar(10),
+    modelo varchar(50),
+    precio float
 );
 
-#drop table Usuarios;
-#truncate Usuarios;
-
-use prueba;
-
-select * from Usuarios;
-
-insert into Usuarios(id, nombre, apellido) values(1, 'juan', 'Rodriguez');
-insert into Usuarios(id, nombre, apellido) values(2, 'Jose', 'Pena');
-insert into Usuarios(id, nombre, apellido) values(3, 'Manolo', 'galvan');
-insert into Usuarios(id, nombre, apellido) values(4, 'Maria', 'valenzuela');
 
 
 
--- sistema_de_registro de clientes/main.py
+++ sistema_de_registro de clientes/main.py
@@ -1,18 +1,18 @@
 import pymysql
-
+import os
 class Database:
     def __init__(self):
         self.connection = pymysql.connect(
             host='localhost',
             user='root',
             password='',
-            db='prueba'
+            db='negocio_de_pulgas'
         )
+        
         self.cursor = self.connection.cursor()
         print("connecion realizada exitosamente")
-
     def select_user(self, id):
-        sql = 'SELECT id, nombre, apellido FROM Usuarios WHERE id = {}'.format(id)
+        sql = 'SELECT Cedula, nombre, apodo FROM Clientes WHERE id = {}'.format(id)
 
         try:
             self.cursor.execute(sql)
@@ -25,80 +25,122 @@ class Database:
 
         except ValueError:
             pass
-
     def select_all_Users(self):
-        sql = 'SELECT id, nombre, apellido FROM Usuarios'
+        sql = 'SELECT cedula, nombre, apodo, Saldo_a_deber FROM Clientes'
         try:
             self.cursor.execute(sql)
             Usuarios = self.cursor.fetchall()
 
             for Usuario in Usuarios:
-                print("id:", Usuario[0])
+                print("cedula:", Usuario[0])
                 print("nombre:", Usuario[1])
-                print("apellido:", Usuario[2])
-                print("___________\n")
+                print("apodo:", Usuario[2])
+                print("Saldo a deber:", Usuario[3])
+                print("-------------------------\n")
 
         except ValueError:
-            pass
-    
+            pass  
     def Delete_register(self):
-        sql = 'TRUNCATE Usuarios'
+        sql = 'TRUNCATE Clientes'
         try:
             self.cursor.execute(sql)
             print("usuarios eliminados correctamente")
         except ValueError:
             pass
-
-    def Update_user(self, id, nombre):
-        sql = "UPDATE Usuarios SET nombre = '{}' WHERE id = {}".format(nombre,id)
+    def Update_Empleados(self, cedula, nombre):
+        sql = "UPDATE Empleados SET nombre = '{}' WHERE cedula = {}".format(nombre,cedula)
         try:
             self.cursor.execute(sql)
             self.connection.commit()
         except ValueError:
-            pass
-    
-    def Insert_Users(self, id, nombre, apellido):
-        sql = "INSERT INTO Usuarios(id, nombre, apellido) VALUES('{}', '{}', '{}')".format(id,nombre,apellido)
+            pass  
+    def Insert_Users(self,nombre, apodo, Cedula):
+        sql = "INSERT INTO Clientes(nombre, apodo, Cedula) VALUES('{}', '{}', '{}')".format(nombre, apodo, Cedula)
         
         try:
             self.cursor.execute(sql)
             
         except ValueError:
             pass
+    def Insert_Products(self, _key, fabricante, modelo, precio):
+        sql = "INSERT INTO productos(_key, fabricante, modelo, precio) VALUES('{}', '{}', '{}', '{}' )".format(_key, fabricante, modelo, precio)
+        try:
+            self.cursor.execute(sql)
+            self.cursor.execute(sql)
+        except ValueError:
+            pass  
+    def Mostrar_Productos(self):
+        sql = "SELECT * FROM productos"
 
-
+        try:
+            self.cursor.execute(sql)
+            Productos = self.cursor.fetchall()
+            for productos in Productos:
+                print("_key", productos[0])
+                print("fabricante:", productos[1])
+                print("modelo:", productos[2])
+                print("precio:", productos[3])
+                print("-------------------------\n")
+        except ValueError:
+            pass 
+    def Insertar_Empleados(self, cedula, nombre, apellido, rango, fecha_de_nacimiento):
+        sql = "INSERT INTO Empleados(cedula, nombre, apellido, rango, fecha_de_nacimiento) VALUES('{}', '{}','{}','{}','{}')".format(cedula,nombre,apellido,rango,fecha_de_nacimiento)
+        try:
+            self.cursor.execute(sql)
+        except ValueError:
+            pass
     def close(self):
         self.connection.close()
         print("conexion termida.")
 
 database = Database() 
 
-persona = str(input("¿En que le podemos servir? \n"))
+encargado = str(input("Bienvedido a negocio de pulgas, ¿en que te podemos servir? \n"))
 
-if persona == "M":
-    database.select_all_Users()
+while True:
 
-elif persona == "C":
-    Cambio_de_dato = str(input("valor a introducir: "))
-    database.Update_user(1, Cambio_de_dato)
+    if encargado == "M":
+        database.select_all_Users()
+        encargado = str(input("Bienvedido a negocio de pulgas, ¿en que te podemos servir? \n"))
 
-elif persona == "P":
-    database.select_user(1)
+    elif encargado == "I":
+        cliente_o_empleado = str(input("desea introducir un nuevo empleado o Cliente: \n"))
+        if cliente_o_empleado == "cliente":
 
-elif persona == "B":
-    database.Delete_register()
-    
-elif persona == "I":
-    id = int(input("inserte ID"))
-    nombre = str(input("inserte Nombre"))
-    apellido = str(input("inserte Apellido"))
-    database.Insert_Users(id,nombre, apellido)
+            nombre = str(input("introducir nombre: "))
+            apodo = str(input("introducir apodo: "))
+            cedula = int((input("introducir su cedula: ")))
 
-    print("usuario agragado con exito", "\n",
-    "Id:", id, "\n", "Nombre:", nombre, "\n" "Apellido:", apellido, "\n")
-    print("------------------------")
-elif persona == "C":
-    database.close()
+            database.Insert_Users(nombre,apodo,cedula)
+            encargado = str(input("Bienvedido a negocio de pulgas, ¿en que te podemos servir? \n"))
 
-elif persona == "A":
-    print("I para insertar, B para borrar, para P mostrar el primer valor, M para mostrar todo")
+        elif cliente_o_empleado == "empleado":
+            cedula = int(input("introducir cedula: "))
+            nombre = str(input("introducir nombre: "))
+            apellido = str(input("introducir apellido: "))
+            rango = str(input("introducir el rango: "))
+            fecha_de_nacimiento = str(input("introducir fecha de nacimiento: "))
+
+            database.Insertar_Empleados(cedula,nombre,apellido, rango, fecha_de_nacimiento)
+
+            encargado = str(input("Bienvedido a negocio de pulgas, ¿en que te podemos servir? \n"))
+        else:
+            print("nesecita ayuda")
+            encargado = str(input("Bienvedido a negocio de pulgas, ¿en que te podemos servir? \n"))
+
+    elif encargado == "IP":
+        _key = str(input("Introducir la key: "))
+        fabricante = str(input("Introducir la marca: "))
+        modelo = str(input("Introducir el modelo: "))
+        precio = str(input("Introducir el precio: "))
+
+        database.Insert_Products(_key,fabricante,modelo, precio)
+        encargado = str(input("Bienvedido a negocio de pulgas, ¿en que te podemos servir? \n"))
+
+    elif encargado == "P":
+        database.Mostrar_Productos()
+        encargado = str(input("Bienvedido a negocio de pulgas, ¿en que te podemos servir? \n"))
+
+    elif encargado == "exit":
+        os.close(exit())
+        database.close()
