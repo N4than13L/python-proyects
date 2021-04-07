f3ab810f37fa2650de704bd0ec2589e7df88ab6f
= <=>
2021-04-06 21:39:35 -0400

    agregando detalles a python downloader

--- python downloader/main.py
+++ python downloader/main.py
@@ -1,8 +1,10 @@
 import pytube
 import os
 
-video_url = input('indroducir la url del video a descargar: ')
-formato = input('seleccione el formato para la descarga: \n a) Audio \n b) Video: ')
+print("Bienvenid@ a Python Downloader.")
+#\n Para salir escriba salir \n Para ayuda escriba h"
+print("Author: José Nathaniel Bonilla")
+print("----------------------------------------------------------")
 
 class YouTube_Downloader:
     def descargar_Videos(self, video_url):
@@ -12,25 +14,30 @@ class YouTube_Downloader:
 
         youtube.streams.first().download(r'Videos')
         print('video descargado exitosamente', youtube.title)
-
+        print("------------------------------------------------------------------")
+        
     def descargar_Audio(self,video_url):
         youtube = pytube.YouTube(video_url)
         print('descargando... espere unos minutos')
 
         youtube.streams.first().download(r'Audios')
         print('audio descargado exitosamente', youtube.title)
+        print("------------------------------------------------------------------")
+
+def Download(): 
+
+    video_url = input('indroducir la url del video a descargar: ')
+    formato = str(input('seleccione el formato para la descarga: \n a) Audio \n b) Video: '))   
+
+    while True:
+        if formato == 'a':
+            video = YouTube_Downloader().descargar_Audio(video_url)
+            break
+        elif formato == 'b':
+            video = YouTube_Downloader().descargar_Videos(video_url)
+            break
 
 while True:
-    if formato == 'a':
-        video = YouTube_Downloader().descargar_Audio(video_url)
-
-video_url = input('indroducir la url del video a descargar: ')
-formato = input('seleccione el formato para la descarga: \n a) Audio \n b) Video: ')
-
-    elif formato == 'b':
-        audio = YouTube_Downloader().descargar_Videos(video_url)
-        
-video_url = input('indroducir la url del video a descargar: ')
-formato = input('seleccione el formato para la descarga: \n a) Audio \n b) Video: ')
-        
-    
+    Download()
+    Download()
+   
